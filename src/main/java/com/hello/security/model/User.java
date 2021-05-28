41ece183a0f06a7d0b3606efedd71a5f8c5fa081
Manish Gupta <manish@respond.com>
2021-05-28 12:32:03 +0530

    Added OpenAPI

--- src/main/java/com/hello/security/model/User.java
+++ src/main/java/com/hello/security/model/User.java
@@ -8,7 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
